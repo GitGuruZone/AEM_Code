6ec7679418c62a4cbcf71f85aa013209d26bc6b2
Sahil Kumar <sahilkumaryogesh@gmail.com>
2024-04-30 18:36:51 +0530

    testcustom

---
+++ core/src/main/java/com/training/projects/core/models/TestCustom.java
@@ -0,0 +1,18 @@
+package com.training.projects.core.models;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = SlingHttpServletRequest.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class TestCustom {
+
+
+    @ValueMapValue
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+}
